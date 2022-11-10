556314cc1569a660193279b432e9d085320a2e45
AniaTofik <anna.bodal@op.pl>
2022-11-10 17:41:21 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'first-project';
+
+  taskName: string = '';
+  config: { [key: string] : string } | null = null;
+  tasks: Task[] = [{
+    name: 'Siłownia',
+    deadLine: '2022-12-12',
+    done: false
+  },
+{
+  name: 'Nauka Angulara',
+  deadLine: '2022-12-26',
+  done: false
+},
+{
+  name: 'Sprzątanie domu',
+  deadLine: '2023-0112',
+  done: false
+}];
+
+  constructor(){
+    this.config = {
+      title: 'Lista zadań',
+      footer: `© Anna's Kiedel ToDoApp.`,
+      date: new Date().toDateString()
+    };
+  }
+  clearTasks() {
+    this.tasks = [];
+  }
+  onKeyUp(event : KeyboardEvent){
+    const target = event.target as HTMLInputElement;
+    this.taskName = target.value;
+  }
+  createTask(){
+    const task: Task = {
+      name: this.taskName,
+      deadLine: '2022-11-25',
+      done: false,
+    };
+    this.tasks.push(task);
+
+  }
 }
